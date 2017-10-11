66ff0fc19574fc4f3799587ae63262159e4404b7
DS-Minetest <vorunbekannt75@web.de>
2017-10-11 17:53:06 +0200

    extend

--- init.lua
+++ init.lua
@@ -12,22 +12,39 @@ local modname = minetest.get_current_modname()
 
 
 local on = true
-local own_name = ""
+local own_names = {}
 local blocked = {}
 
 local function get_msg(msg)
-	if msg:sub(1, 1) ~= "<" then
-		return msg
+	local name
+	if msg:sub(1, 1) == "*" then -- /me
+		local f = msg:find(" ", 3)
+		if not f then
+			return msg
+		end
+		local name = msg:sub(3, f-1)
+		msg = msg:sub(f+1)
+	elseif msg:sub(1, 8) == "PM from " then -- /msg
+		local f = msg:find(":", 9)
+		if not f then
+			return msg
+		end
+		local name = msg:sub(9, f-1)
+		msg = msg:sub(f+2)
+	else -- normal chat
+		if msg:sub(1, 1) ~= "<" then
+			return msg
+		end
+		local f = msg:find(">")
+		if not f then
+			return msg
+		end
+		name = msg:sub(2, f-1)
+		msg = msg:sub(f+2)
 	end
-	local f = msg:find(">")
-	if not f then
-		return msg
-	end
-	local name = msg:sub(2, f-1)
-	if blocked[name] then
+	if blocked[name] or own_names[name] then
 		return false
 	end
-	msg = msg:sub(f+1) -- The space before is wanted.
 	return msg
 end
 
@@ -37,8 +54,7 @@ local function is_special_char(c)
 end
 
 minetest.register_on_connect(function()
-	own_name = minetest.localplayer:get_name()
-	blocked[own_name] = true
+	own_names[minetest.localplayer:get_name()] = true
 end)
 
 minetest.register_on_receiving_chat_message(function(omsg)
@@ -46,17 +62,28 @@ minetest.register_on_receiving_chat_message(function(omsg)
 	if not msg then
 		return
 	end
-	local lmsg, loname = msg:lower(), own_name:lower()
-	for i = 1, #lmsg do
-		local f
-		f, i = lmsg:find(loname, i)
-		if not f then
-			return
+	local lmsg = " "..msg:lower()
+	local ok = false
+	for own_name, b in pairs(own_names) do
+		local loname = own_name:lower()
+		for i = 1, #lmsg do
+			local f
+			f, i = lmsg:find(loname, i)
+			if not f then
+				break
+			end
+			if is_special_char(lmsg:sub(f-1, f-1)) and
+					(is_special_char(lmsg:sub(i+1, i+1)) or i >= #lmsg) then
+				ok = true
+				break
+			end
 		end
-		if is_special_char(lmsg:sub(f-1, f-1)) and
-				(is_special_char(lmsg:sub(i+1, i+1)) or i >= #lmsg) then
+		if ok then
 			break
 		end
+	end
+	if not ok then
+		return
 	end
 	minetest.sound_play("chatbell_bell", {gain = 0.5})
 	minetest.display_chat_message(minetest.colorize("#52DA2D", omsg))
@@ -64,7 +91,7 @@ minetest.register_on_receiving_chat_message(function(omsg)
 end)
 
 minetest.register_chatcommand("chatbell", {
-	params = "get | toggle | block <player>",
+	params = "get | toggle | block <player> | name <nickname>",
 	description = "Control the chatbell:n"..
 	"  toggle: Toggle the bell on/off. (Default)\n"..
 	"  block <player>: Block/unblock a specific player.",
@@ -89,11 +116,20 @@ minetest.register_chatcommand("chatbell", {
 			blocked[param] = not blocked[param]
 			return true, param.." "..((blocked[param] and "") or "not ").."blocked."
 
+		elseif cmd == "name" then
+			own_names[param] = not own_names[param] or nil
+			return true, "Nickname "..param.." toggled "..
+				(own_names[param] and "on" or "off").."."
+
 		elseif cmd == "get" then
 			if not on then
 				return true, "Chatbell is disabled."
 			end
-			return true, "Blocked players: "..dump(blocked)
+			local msg = "Blocked players: "..dump(blocked).."Names:"
+			for name, b in pairs(own_names) do
+				msg = msg.."\n"..name
+			end
+			return true, msg
 
 		else
 			return false, "Invalid arguments."
